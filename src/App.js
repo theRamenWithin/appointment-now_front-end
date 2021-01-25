0e2a173d2bc7111494d22431ac35d786eea64550
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-01-25 20:00:49 +1100

    Compacted

--- src/App.js
+++ src/App.js
@@ -1,93 +1,16 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-
-import logoSmall from './assets/logo-small.svg';
 import './App.css';
 
+import NavBar from './components/NavBar';
+import SimpleContainer from './components/SimpleContainer';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <nav className="navbar">
-          <div className="nav-left">
-            <img src={logoSmall} alt="Logo typeface"></img>
-            <ul className="nav-buttons">
-              <li>
-                <Link to="/signup">Sign Up</Link>
-              </li>
-              <li>
-                <Link to="/signin">Sign In</Link>
-              </li>
-            </ul>
-          </div>
-          
-          <div className="nav-right">
-            <ul className="nav-buttons">
-              <li>
-                <Link to="/">About</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact Us</Link>
-              </li>
-              <li>
-                <Link to="/blog">Blog</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-
-        <Switch>
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-          <Route path="/signin">
-            <SignIn />
-          </Route>
-          <Route path="/blog">
-            <Blog />
-          </Route>
-          <Route path="/Contact">
-            <Contact />
-          </Route>
-          <Route path="/">
-            <About />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <NavBar />
+      <SimpleContainer />
+      <Footer />
+    </>
   );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-      <FontAwesomeIcon icon={faSearch} />
-    </div>
-  );
-}
-
-function Contact() {
-  return <h2>Contact Us</h2>;
-}
-
-function Blog() {
-  return <h2>Blog</h2>;
-}
-
-function SignUp() {
-  return <h2>SignUp</h2>;
-}
-
-function SignIn() {
-  return <h2>Sign In</h2>;
 }
