3022c9b5c3ddbb18b2c71c7c25f8f89c2819c4be
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-01-26 15:58:54 +1100

    Moved Routing into App

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,5 @@
 import React from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-
-import About from './About';
-import Contact from './Contact';
-import Blog from './Blog';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
+import { Link } from "react-router-dom";
 
 import logoSmall from '../assets/logo-small.svg';
 
@@ -18,34 +7,22 @@ import logoSmall from '../assets/logo-small.svg';
 
 export default function NavBar() {
     return (
-        <Router>
-        <div>
-          <nav className="nav">
-            <div className="nav-left">
-              <img src={logoSmall} alt="Logo typeface"></img>
-              <ul>
-                <li><Link to={'/signout'} className="nav-link">Sign Up</Link></li>
-                <li><Link to={'/signin'} className="nav-link">Sign In</Link></li>
-              </ul>
-            </div>
-            
-            <div className="nav-right">
-              <ul>
-                <li><Link to={'/'} className="nav-link">About</Link></li>
-                <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
-                <li><Link to={'/blog'} className="nav-link">Blog</Link></li>
-              </ul>
-            </div>
-          </nav>
-  
-          <Switch>
-            <Route exact path='/signup' component={SignUp} />
-            <Route exact path='/signin' component={SignIn} />
-            <Route exact path='/blog' component={Blog} />
-            <Route exact path='/contact' component={Contact} />
-            <Route exact path='/' component={About} />
-          </Switch>
+      <nav className="nav">
+        <div className="nav-left">
+          <img src={logoSmall} alt="Logo typeface"/>
+          <ul>
+            <li><Link to={'/signup'} className="nav-link">Sign Up </Link></li>
+            <li><Link to={'/signin'} className="nav-link"> Sign In</Link></li>
+          </ul>
         </div>
-      </Router>
+        
+        <div className="nav-right">
+          <ul>
+            <li><Link to={'/'} className="nav-link">About </Link></li>
+            <li><Link to={'/contact'} className="nav-link"> Contact </Link></li>
+            <li><Link to={'/blog'} className="nav-link"> Blog</Link></li>
+          </ul>
+        </div>
+      </nav>
     );
 }
