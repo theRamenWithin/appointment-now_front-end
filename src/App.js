c0ff4590a4eb9100af1f782786526d362ecc0963
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-01-26 15:59:19 +1100

    Imported Routing and Container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from 'react'
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+// Components
 import NavBar from './components/NavBar';
-import SimpleContainer from './components/SimpleContainer';
 import Footer from './components/Footer';
+
+// Page content
+import About from './components/About';
+import Contact from './components/Contact';
+import Blog from './components/Blog';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import NotFound from './components/404.js';
+
+// Styling
+import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 export default function App() {
   return (
     <>
-      <NavBar />
-      <SimpleContainer />
-      <Footer />
+      <Router>
+        <div>
+          <CssBaseline />
+          <NavBar />
+          <Container maxWidth="lg">
+            <Typography component="div" style={{ backgroundColor: '#FFF', borderLeft: '2vw solid #4EC5E6', borderRight: '2vw solid #4EC5E6', height: '100vh', padding: '20px' }}>
+              <Switch>
+                <Route exact path='/signup' component={SignUp} />
+                <Route exact path='/signin' component={SignIn} />
+                <Route exact path='/blog' component={Blog} />
+                <Route exact path='/contact' component={Contact} />
+                <Route exact path='/' component={About} />
+                <Route component={NotFound}/>
+              </Switch>
+            </Typography>
+          </Container>
+          <Footer />
+        </div>
+      </Router>
     </>
   );
 }
