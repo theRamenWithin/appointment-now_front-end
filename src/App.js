8061200203dc3a25c52c00d1e84451ceee5d7f60
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-01-28 09:38:21 +1100

    Added sidebar links and refactored containers

--- src/App.js
+++ src/App.js
@@ -11,32 +11,39 @@ import Contact from './components/Contact';
 import Blog from './components/Blog';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+
+// Sidebar links
+import Events from './components/Events';
+import EditProfile from './components/EditProfile';
+import EditOrganisation from './components/EditOrganisation';
+
 import NotFound from './components/404.js';
 
 // Styling
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
 export default function App() {
   return (
     <>
       <Router>
-        <div>
+        <div className="super-container">
           <CssBaseline />
           <NavBar />
-          <Container maxWidth="lg">
-            <Typography component="div" style={{ backgroundColor: '#FFF', borderLeft: '2vw solid #4EC5E6', borderRight: '2vw solid #4EC5E6', height: '100vh', padding: '20px' }}>
-              <Switch>
-                <Route exact path='/signup' component={SignUp} />
-                <Route exact path='/signin' component={SignIn} />
-                <Route exact path='/blog' component={Blog} />
-                <Route exact path='/contact' component={Contact} />
-                <Route exact path='/' component={About} />
-                <Route component={NotFound}/>
-              </Switch>
-            </Typography>
+          <Container maxWidth="lg" className="container">
+            <Switch>\
+              <Route exact path='/events' component={Events} />
+              <Route exact path='/editprofile' component={EditProfile} />
+              <Route exact path='/editorganisation' component={EditOrganisation} />
+
+              <Route exact path='/signup' component={SignUp} />
+              <Route exact path='/signin' component={SignIn} />
+              <Route exact path='/blog' component={Blog} />
+              <Route exact path='/contact' component={Contact} />
+              <Route exact path='/' component={About} />
+              <Route component={NotFound}/>
+            </Switch>
           </Container>
           <Footer />
         </div>
