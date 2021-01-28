2c532926dd67eed22c9e412c4fa729b90fe4eafa
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-01-28 16:53:57 +1100

    Added logic for logging out

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,41 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 import { Link } from "react-router-dom";
 
 import logoSmall from '../assets/logo-small.svg';
 
-// import { AppBar, Toolbar } from '@material-ui/core'
+export default function NavBar(props) {
+  const handleClick = () => {
+    axios.delete('http://localhost:3001/logout', {withCredentials: true})
+    .then(response => {
+      props.handleLogout()
+      props.history.push('/')
+    })
+    .catch(error => console.log(error))
+  }
 
-export default function NavBar() {
-    return (
-      <nav className="nav">
-        <div className="nav-left">
-          <img src={logoSmall} alt="Logo typeface"/>
-          <ul>
-            <li><Link to={'/signup'} className="nav-link">Sign Up </Link></li>
-            <li><Link to={'/signin'} className="nav-link"> Sign In</Link></li>
-          </ul>
-        </div>
-        
-        <div className="nav-right">
-          <ul>
-            <li><Link to={'/'} className="nav-link">About </Link></li>
-            <li><Link to={'/contact'} className="nav-link"> Contact </Link></li>
-            <li><Link to={'/blog'} className="nav-link"> Blog</Link></li>
-          </ul>
-        </div>
-      </nav>
-    );
+  return (
+    <nav className="nav">
+      <div className="nav-left">
+        <img src={logoSmall} alt="Logo typeface"/>
+        <ul>
+          <li><Link to={'/signup'} className="nav-link">Sign Up </Link></li>
+          <li><Link to={'/signin'} className="nav-link"> Sign In</Link></li>
+          <li>
+            {
+              props.loggedInStatus ? <Link to={'/logout'} onClick={handleClick}>Log Out</Link> : null
+            }
+          </li>
+        </ul>
+      </div>
+      
+      <div className="nav-right">
+        <ul>
+          <li><Link to={'/'} className="nav-link">About </Link></li>
+          <li><Link to={'/contact'} className="nav-link"> Contact </Link></li>
+          <li><Link to={'/blog'} className="nav-link"> Blog</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
