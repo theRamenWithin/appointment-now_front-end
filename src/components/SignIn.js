b4773b1e7aadff442922f8fd4ae78fe89d257911
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-01-28 16:54:15 +1100

    Added logic for signing in with token

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, useHistory } from "react-router-dom";
 
+// Styling
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -10,7 +12,9 @@ import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
+// Logos
 import logoLarge from '../assets/logo-large.svg';
+import OR from '../assets/OR.svg';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -19,38 +23,103 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
   },
-
   form: {
     width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    margin: theme.spacing(2, 0, 3),
   },
 }));
 
-export default function SignIn() {
+export default function SignIn(props) {
   const classes = useStyles();
+  const history = useHistory();
+  
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // const {username, email, password, password_confirmation} = this.state
+    let user = {
+      username: username,
+      email: password,
+      password: password
+    }
+
+  axios.post('http://localhost:3000/login', {user}, {withCredentials: true})
+    .then(response => {
+      if (response.data.logged_in) {
+        props.handleLogin(response.data)
+        redirect()
+      } else {
+        setErrors(response.data.errors)
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  };
+
+  const redirect = () => {
+    history.push('/')
+  }
+
+  const handleErrors = () => {
+    return (
+      <div>
+        <ul>
+          {errors.map(error => {
+            return <li key={error}>{error}</li>
+          })}
+        </ul>
+      </div>
+    )
+  }
 
   return (
     <Container component="main" maxWidth="sm" className="curved-container with-logo">
       <CssBaseline />
+
+      {/* Logo */}
       <div className="logo">
         <img src={logoLarge} alt="Logo large"/>
       </div>
       
       <div className={classes.paper}>
-        <form className={classes.form} noValidate>
+        {/* Form */}
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
-                required
                 fullWidth
-                id="email"
+                label="Username"
+                name="username"
+                value={username}
+                autoComplete="username"
+                onChange={e => setUsername(e.target.value)}
+                InputProps={{
+                    style: {
+                      backgroundColor: "white"
+                    },
+                }}
+              />
+            </Grid>
+
+            <img src={OR} alt="OR line"/>
+
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                fullWidth
                 label="Email Address"
                 name="email"
+                value={email}
                 autoComplete="email"
+                onChange={e => setEmail(e.target.value)}
                 InputProps={{
                     style: {
                       backgroundColor: "white"
@@ -65,8 +134,9 @@ export default function SignIn() {
                 fullWidth
                 name="password"
                 label="Password"
+                value={password}
                 type="password"
-                id="password"
+                onChange={e => setPassword(e.target.value)}
                 autoComplete="current-password"
                 InputProps={{
                     style: {
@@ -74,12 +144,16 @@ export default function SignIn() {
                     },
                 }}
               />
+            </Grid>
+          
+            <Grid item xs={12}>
               <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
               />
             </Grid>
           </Grid>
+
           <Button
             type="submit"
             fullWidth
@@ -89,17 +163,21 @@ export default function SignIn() {
           >
             Sign In
           </Button>
+
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
+              <Link to={'/resetpassword'} variant="body2">Forgot password?</Link>
             </Grid>
             <Grid item>
               Don't have an account? <Link to={'/signup'} variant="body2">Sign Up</Link>
             </Grid>
           </Grid>
         </form>
+        <div>
+          {
+            errors ? handleErrors() : null
+          }
+        </div>
       </div>
     </Container>
   );
