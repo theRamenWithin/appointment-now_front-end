c96ac9e3d10e71ad5c6a4c38b64a857aff4e0ecb
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2021-01-29 17:23:22 +1100

    Updated state handling and modal content

--- src/components/EditOrganisation.js
+++ src/components/EditOrganisation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
@@ -9,6 +9,8 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import { DropzoneArea } from 'material-ui-dropzone';
 
+import Banner from '../assets/modal-org-splash.jpg';
+
 const useStyles = makeStyles((theme) => ({
   modal: {
     display: 'flex',
@@ -18,8 +20,20 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     backgroundColor: theme.palette.background.paper,
     border: '2px solid #000',
+    borderRadius: '15px',
+    width: '70vw',
+    height: '95vh',
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
+  },
+  banner: {
+    width: '100%',
+    maxHeight: '150px',
+    overflow: 'hidden',
+    borderRadius: '12px 12px 0 0',
+  },
+  modaladdress: {
+    backgroundColor: 'rgba(78, 197, 230, 0.20)',
+    height: '100%',
   },
   form: {
     width: '100%',
@@ -32,7 +46,24 @@ const useStyles = makeStyles((theme) => ({
 
 export default function EditOrganisation() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [values, setValues] = useState({
+    name: '',
+    description: '',
+    address1: '',
+    address2: '',
+    city: '',
+    state: '',
+    postcode: '',
+    country: '',
+    telephone: '',
+    email: '',
+    image: ''
+  });
+
+  const handleChange = (e) => {
+    setValues({...values, [e.target.name]: e.target.value})
+  }
 
   const handleOpen = () => {
     setOpen(true);
@@ -56,9 +87,10 @@ export default function EditOrganisation() {
               variant="outlined"
               required
               fullWidth
-              id="name"
               label="Organisation Name"
               name="name"
+              value={values.name}
+              onChange={handleChange}
               InputProps={{
                   style: {
                     backgroundColor: "white"
@@ -73,9 +105,10 @@ export default function EditOrganisation() {
               fullWidth
               multiline
               rows={4}
-              id="description"
               label="Organisation Description"
               name="description"
+              value={values.description}
+              onChange={handleChange}
               InputProps={{
                   style: {
                     backgroundColor: "white"
@@ -90,9 +123,10 @@ export default function EditOrganisation() {
               variant="outlined"
               required
               fullWidth
-              id="address1"
               label="Address Line 1"
               name="address1"
+              value={values.address1}
+              onChange={handleChange}
               InputProps={{
                   style: {
                     backgroundColor: "white"
@@ -104,9 +138,10 @@ export default function EditOrganisation() {
             <TextField
               variant="outlined"
               fullWidth
-              id="address2"
               label="Address Line 2"
               name="address2"
+              value={values.address2}
+              onChange={handleChange}
               InputProps={{
                   style: {
                     backgroundColor: "white"
@@ -119,9 +154,10 @@ export default function EditOrganisation() {
               variant="outlined"
               required
               fullWidth
-              id="city"
               label="City"
               name="city"
+              value={values.city}
+              onChange={handleChange}
               InputProps={{
                   style: {
                     backgroundColor: "white"
@@ -134,9 +170,10 @@ export default function EditOrganisation() {
               variant="outlined"
               required
               fullWidth
-              id="state"
               label="State/Province"
               name="state"
+              value={values.state}
+              onChange={handleChange}
               InputProps={{
                   style: {
                     backgroundColor: "white"
@@ -148,9 +185,10 @@ export default function EditOrganisation() {
             <TextField
               variant="outlined"
               fullWidth
-              id="postcode"
               label="Postcode"
               name="postcode"
+              value={values.postcode}
+              onChange={handleChange}
               InputProps={{
                   style: {
                     backgroundColor: "white"
@@ -163,9 +201,10 @@ export default function EditOrganisation() {
               variant="outlined"
               required
               fullWidth
-              id="country"
               label="Country"
               name="country"
+              value={values.country}
+              onChange={handleChange}
               InputProps={{
                   style: {
                     backgroundColor: "white"
@@ -180,24 +219,10 @@ export default function EditOrganisation() {
               variant="outlined"
               required
               fullWidth
-              id="country"
-              label="Country"
-              name="country"
-              InputProps={{
-                  style: {
-                    backgroundColor: "white"
-                  },
-              }}
-            />
-          </Grid>
-          <Grid item xs={10}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="telephone"
               label="Telephone No."
               name="telephone"
+              value={values.telephone}
+              onChange={handleChange}
               InputProps={{
                   style: {
                     backgroundColor: "white"
@@ -210,9 +235,10 @@ export default function EditOrganisation() {
               variant="outlined"
               required
               fullWidth
-              id="email"
               label="Email Address"
               name="email"
+              value={values.email}
+              onChange={handleChange}
               InputProps={{
                   style: {
                     backgroundColor: "white"
@@ -223,9 +249,10 @@ export default function EditOrganisation() {
           {/* Banner Image */}
           <Grid item xs={10}>
           <DropzoneArea
+            name="image"
             acceptedFiles={['image/*']}
             dropzoneText={"Upload a banner image. Drag and drop an image here or click"}
-            onChange={(files) => console.log('Files:', files)}
+            onChange={handleChange}
           />
           </Grid>
         </Grid>
@@ -275,8 +302,35 @@ export default function EditOrganisation() {
       >
         <Fade in={open}>
           <div className={classes.paper}>
-            <h2 id="transition-modal-title">Title</h2>
-            <p id="transition-modal-description">description</p>
+            <Grid container spacing={2} >
+              <Grid item xs={12}>
+                <img src={Banner} alt="org banner" className={classes.banner}></img>
+              </Grid>
+              <Grid item xs={4} className={classes.modaladdress}>
+              <iframe
+                title="Organisation Location"
+                width="500"
+                height="500"
+                frameborder="0"
+                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDsw_-i9Nf1d77G0BBtsNT6TIbPk2tHEZQ
+                  &q=Sydney">
+              </iframe>
+                <ul>
+                  <li>Address Line 1: {values.address1}</li>
+                  <li>Addres Line 2: </li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </Grid>
+              <Grid item xs={8}>
+                
+              </Grid>
+            </Grid>
           </div>
         </Fade>
       </Modal>
