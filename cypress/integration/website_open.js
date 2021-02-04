3fbce86c3a531d11f39095831cac5ca826b1a93c
Gavin Simms <me@gavinsimms.com>
2021-02-04 14:39:35 +1100

    added first cypress test website open

---
+++ cypress/integration/website_open.js
@@ -0,0 +1,14 @@
+describe('My First Test', function () {
+    it('Tests to see if the website loads', function () {
+        cy.visit('http://localhost:3000/')
+    })
+  })
+  
+  // Test setup to have three phases:
+  
+  // Arrange - setup initial app state
+  
+  // Act - take an action
+  
+  // Assert - make an assertion
+  
